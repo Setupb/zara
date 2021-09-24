29fd0f53a9b8ea8cec4029c0927bfb52e4549dfa
Yurii Kylyn <89136377+Setupb@users.noreply.github.com>
2021-09-24 11:05:23 +0300

    Create  Copyright.jsx

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Copyright = function(){
+    return(
+        <div className='footer_Copyright'>
+            <p>2020 &copy; IoT Copyright all rights reserved, bla bla</p>
+        </div>
+    )
+}
+export default Copyright
