05074114716c85257fc7e7c52eddda48f8106fa5
Yurii Kylyn <89136377+Setupb@users.noreply.github.com>
2021-09-28 01:26:41 +0300

    Create MainHeading.js

---
+++ src/components/Home/MainHeading.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+
+const MainHeading = () => {
+    return (
+        <div className="main-header">
+            <div className="main-header__img"/>
+            <div className="main-header-body">
+                <h1>Zara Espana S.A. (Zara)</h1>
+                <p className="main-header__text">
+                    Zara is one of the world's largest international fashion companies .
+                    It belongs to Inditex, the world’s biggest fashion group.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default MainHeading;
